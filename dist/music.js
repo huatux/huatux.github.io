e23520bef4d1ca7ad575aca01b5be19147ef7edc
huatux <lin1985@gmail.com>
2023-03-25 15:21:17 +0000

    Site updated: 2023-03-25 15:21:15

---
+++ dist/music.js
@@ -0,0 +1,43 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+	  {
+        name: 'One More Light',
+        artist: 'Linkin Park',
+        url: 'https://huatux.github.io/music/OneMoreLight.mp3',
+        cover: 'http://p2.music.126.net/O_UyuQ9Dzvu4SkjKX3KVDQ==/109951163931119301.jpg?param=130y130',
+      },
+	  {
+        name: 'Skydance',
+        artist: 'W&W/AXMO/Giin',
+        url: 'https://huatux.github.io/music/Skydance.m4a',
+        cover: 'https://p2.music.126.net/4t5fZU-tqPG2YO48xxLxlg==/109951165912664481.jpg?param=130y130',
+      },
+      {
+        name: 'Are We Alone',
+        artist: 'NIVIRO',
+        url: 'https://huatux.github.io/music/AreWeAlone.m4a',
+        cover: 'https://p2.music.126.net/P9UJmcItd4yO6HNL0UEsDw==/109951164608866014.jpg?param=130y130',
+      },
+      {
+        name: 'Visions',
+        artist: 'Charli XCX',
+        url: 'https://huatux.github.io/music/visions.m4a',
+        cover: 'https://p2.music.126.net/ySEhPjaLrkRvaigtrX1xyA==/109951164987523321.jpg?param=130y130',
+      },
+      {
+        name: 'With or Without You',
+        artist: 'Max Oazo/Camishe',
+        url: 'https://huatux.github.io/music/WithorWithoutYou.m4a',
+        cover: 'https://p2.music.126.net/1Pg8P7RmPg3zRxZe7mm20Q==/109951164524491658.jpg?param=130y130',
+      },
+      {
+        name: 'Are You With Me',
+        artist: 'Lost Frequencies',
+        url: 'https://huatux.github.io/music/AreYouWithMe.m4a',
+        cover: 'https://p1.music.126.net/dc_kyRDAgRgKKbdJjy26Nw==/109951163118657056.jpg?param=130y130',
+      }
+    ]
+});
